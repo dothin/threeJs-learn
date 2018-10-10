19a7300e075420bb4e100e771def99ba046855d4
gaoHB <1286513426@qq.com>
2018-10-10 14:28:51 +0800

    update( project ):加载3D模型

--- src/index.js
+++ src/index.js
@@ -5,30 +5,141 @@
  * Time: 下午4:36
  */
 
-import * as THREE from 'three/build/three.module.js';
-
 console.log(THREE);
 
 document.body.style.margin = 0;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+var container, stats, controls;
+var camera, scene, renderer, light;
 
-var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
+var clock = new THREE.Clock();
 
-var scene = new THREE.Scene();
+var mixers = [];
 
-var material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+init();
+animate();
 
-var geometry = new THREE.Geometry();
-geometry.vertices.push(new THREE.Vector3( -10, 0, 0) );
-geometry.vertices.push(new THREE.Vector3( 0, 10, 0) );
-geometry.vertices.push(new THREE.Vector3( 10, 0, 0) );
+function init() {
 
-var line = new THREE.Line( geometry, material );
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
 
-scene.add( line );
-renderer.render( scene, camera );
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+	camera.position.set( 100, 200, 300 );
+
+	controls = new THREE.OrbitControls( camera );
+	controls.target.set( 0, 100, 100 );
+	controls.update();
+
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color( 0x000000 );
+	scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
+
+	light = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+	light.position.set( 0, 200, 0 );
+	scene.add( light );
+
+	light = new THREE.DirectionalLight( 0xffffff );
+	light.position.set( 0, 200, 100 );
+	light.castShadow = true;
+	light.shadow.camera.top = 180;
+	light.shadow.camera.bottom = -100;
+	light.shadow.camera.left = -120;
+	light.shadow.camera.right = 120;
+	scene.add( light );
+
+	// scene.add( new THREE.CameraHelper( light.shadow.camera ) );
+
+	// ground
+	var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+	mesh.rotation.x = - Math.PI / 2;
+	mesh.receiveShadow = true;
+	scene.add( mesh );
+
+	var grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
+	grid.material.opacity = 0.2;
+	grid.material.transparent = true;
+	scene.add( grid );
+
+	// model
+	var loader = new THREE.FBXLoader();
+	loader.load( './models/fbx/zonghe.fbx', function ( object ) {
+		console.log(object);
+		object.scale.multiplyScalar(2);
+		scene.add( object );
+	} );
+
+	for(var i = 0; i < 12; i++) {
+
+		loader.load( `./models/fbx/student${i}.FBX`, function ( object ) {
+			console.log(object);
+			object.scale.multiplyScalar(2);
+			scene.add( object );
+		} );
+
+	}
+    /*
+	for(var i = 0; i < 4; i++) {
+
+		loader.load( `./models/fbx/${i}.FBX`, function ( object ) {
+			console.log(object);
+			object.scale.multiplyScalar(2);
+			scene.add( object );
+		} );
+
+	}*/
+
+	for(var i = 0; i < 3; i++) {
+
+		loader.load( `./models/fbx/shitang${i}.FBX`, function ( object ) {
+			console.log(object);
+			object.scale.multiplyScalar(2);
+			scene.add( object );
+		} );
+
+	}
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.shadowMap.enabled = true;
+	container.appendChild( renderer.domElement );
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+	// stats
+	stats = new Stats();
+	container.appendChild( stats.dom );
+
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+//
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	if ( mixers.length > 0 ) {
+
+		for ( var i = 0; i < mixers.length; i ++ ) {
+
+			mixers[ i ].update( clock.getDelta() );
+
+		}
+
+	}
+
+	renderer.render( scene, camera );
+
+	stats.update();
+
+}
