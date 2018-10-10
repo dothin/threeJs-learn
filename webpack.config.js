0570e9e150c714fbaf69c5bc90ad7201e8c135d4
gaoHB <1286513426@qq.com>
2018-10-10 14:27:47 +0800

    update( webpack.config ):调整构建

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@
 const path               = require('path');
 const HtmlWebpackPlugin  = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+let CopyWebpackPlugin       = require('copy-webpack-plugin');
 
 module.exports = {
 	mode        : "development",
@@ -18,11 +19,44 @@ module.exports = {
 	devServer   : {
 		contentBase: "./dist"
 	},
+	module      : {
+		rules: [
+			{
+				test: require.resolve("three/examples/js/controls/OrbitControls"),
+				use : "imports-loader?THREE=three"
+			},
+			{
+				test: require.resolve("three/examples/js/controls/OrbitControls"),
+				use : "exports-loader?THREE.OrbitControls"
+			},
+			{
+				test: require.resolve("three/examples/js/loaders/FBXLoader"),
+				use : "imports-loader?THREE=three"
+			},
+			{
+				test: require.resolve("three/examples/js/loaders/FBXLoader"),
+				use : "exports-loader?THREE.FBXLoader"
+			}
+		]
+	},
 	plugins     : [
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
-			title: 'Development'
-		})
+			inject        : true,
+			template      : `index.html`,
+		}),
+		new CopyWebpackPlugin([
+			{
+				from  : path.resolve(__dirname, `./static/models`),
+				to    : path.resolve(__dirname, './dist/models'),
+				ignore: ['.*']
+			},
+			{
+				from  : path.resolve(__dirname, `./libs`),
+				to    : path.resolve(__dirname, './dist/libs'),
+				ignore: ['.*']
+			}
+		])
 	],
 	watch       : true,
 	watchOptions: {
